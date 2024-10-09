9bc5fd058fce7e6d8a2adde5320ca391e22bf08e
NuralySol <soltonbekov@gmail.com>
2024-10-09 14:20:17 -0400

    added .env for hiding the scores

--- scrabble.py
+++ scrabble.py
@@ -2,17 +2,15 @@
 #! needs an import and export module importing pandas for matrix and scrabble_checker function 
 #! import also json
 #! from the scrabble_dictionary.py
-
+from dotenv import load_dotenv
 from scrabble_dictionary import scrabble_checker  
 import pandas as pd  
 import os
 import json
 
-scrabble_score_key = {
-    "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8,
-    "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1,
-    "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10
-}
+load_dotenv()
+
+scrabble_score_key = json.loads(os.getenv("SCRABLE_SCORE_KEY"))
 
 valid_scrabble_words = scrabble_checker()  
 
